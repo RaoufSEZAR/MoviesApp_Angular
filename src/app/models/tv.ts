89ddcb382b04690c65d89777cbe86e0ad13c4bac
raoufsezar <raoufsato999@gmail,com>
2022-11-15 22:12:59 +0300

    add tv interface

--- src/app/models/tv.ts
+++ src/app/models/tv.ts
@@ -1,20 +1,7 @@
 import { Movie } from './movie';
 
-export interface Tv {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: number[];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
+export interface Tv extends Movie {
   name: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
 }
 
 export interface TvDto {
