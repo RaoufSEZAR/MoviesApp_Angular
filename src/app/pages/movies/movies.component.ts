4d3160e4e84e2b489f4afd7145643ba417ffc782
raoufsezar <raoufsato999@gmail,com>
2022-11-15 18:52:45 +0300

    movies page works

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from './../../services/movies.service';
+import { Movie } from './../../models/movie';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
 })
 export class MoviesComponent implements OnInit {
-
-  constructor() { }
+  movies: Movie[] = [];
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.moviesService.getMovies('popular', 20).subscribe((movies) => {
+      this.movies = movies;
+    });
   }
-
 }
