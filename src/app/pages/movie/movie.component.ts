f69bc999e766f65e7d2d7630718ddd00904a026a
raoufsezar <raoufsato999@gmail,com>
2022-11-16 17:26:33 +0300

    overview tab has finished

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MoviesService } from './../../services/movies.service';
 import { Movie } from './../../models/movie';
+import { IMAGES_SIZES } from './../../constants/images-sizes';
 
 @Component({
   selector: 'app-movie',
@@ -10,6 +11,7 @@ import { Movie } from './../../models/movie';
 })
 export class MovieComponent implements OnInit {
   movie: Movie | null = null;
+  imagesSizes = IMAGES_SIZES;
   constructor(
     private router: ActivatedRoute,
     private moviesService: MoviesService
